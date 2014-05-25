5e33900b352cd68b6b6a7a33839abad85fc51d5c
André Dieb Martins <andre.dieb@gmail.com>
2014-05-25 13:18:35 -0400

    Refactor generator

--- lib/gemstrap/generator.rb
+++ lib/gemstrap/generator.rb
@@ -1,22 +1,13 @@
 require 'pathname'
 require 'fileutils'
-require 'gemstrap/template'
 
+require 'gemstrap/template'
+require 'gemstrap/colorize'
 
 module Gemstrap
   class Generator
     include FileUtils
-
-    CLEAR   = "\e[0m"
-    BOLD    = "\e[1m"
-    BLACK   = "\e[30m"
-    RED     = "\e[31m"
-    GREEN   = "\e[32m"
-    YELLOW  = "\e[33m"
-    BLUE    = "\e[34m"
-    MAGENTA = "\e[35m"
-    CYAN    = "\e[36m"
-    WHITE   = "\e[37m"
+    include Colorize
 
     def run(args)
       options.merge!(args)
@@ -52,57 +43,40 @@ module Gemstrap
       options[:path]      = Pathname.new('.').join(options[:gem_name])
       options[:lib_path]  = options[:path].join('lib')
       options[:spec_path] = options[:path].join('spec')
-      options[:gem_module] = options[:gem_name].split('-').map { |n| n.split('_') }.flatten.map(&:capitalize).join
+      options[:gem_module] = gem_name_to_module_name
       options
     end
 
     def generate!
       puts "   generate"
       mkdir_p(path)
-      copy_gitignore
-      copy_rakefile
-      copy_gemfile
-      generate_readme
-      generate_gemspec
-      mkdir_p(lib_path)
-      mkdir_p(lib_path.join(gem_name))
+      generate_root_files
       generate_lib_files
       mkdir_p(spec_path)
       cp(templates.join('Rakefile'), path)
     end
 
-    def mkdir_p(path)
-      say_created(path)
-      super(path)
-    end
-
-    def copy_gitignore
-      copy_template(templates.join('gitignore'), path.join('.gitignore'))
-    end
-
-    def copy_rakefile
-      copy_template(templates.join('Rakefile'), path.join('Rakefile'))
-    end
-
-    def copy_gemfile
-      copy_template(templates.join('Gemfile'), path.join('Gemfile'))
-    end
-
-    def generate_readme
+    def generate_root_files
+      %w(.gitignore Rakefile Gemfile).each do |file|
+        copy_template(templates.join(file), path.join(file))
+      end
       generate_file(path.join('README.md'), render_template('README.md.erb'))
-    end
-
-    def generate_gemspec
       generate_file(path.join("#{gem_name}.gemspec"), render_template('gemspec.erb'))
     end
 
     def generate_lib_files
+      [lib_path, lib_path.join(gem_name)].each { |path| mkdir_p(path) }
       generate_file(lib_path.join("#{gem_name}.rb"), render_template('lib/root.rb.erb'))
       generate_file(lib_path.join("#{gem_name}/version.rb"), render_template('lib/gem_name/version.rb.erb'))
     end
 
     def render_template(template_path)
       Gemstrap::Template.render(templates.join(template_path), context)
+    end
+
+    def mkdir_p(path)
+      say_created(path)
+      super(path)
     end
 
     def copy_template(from, to)
@@ -128,9 +102,9 @@ module Gemstrap
       puts "     #{created_color}  #{item.to_s}"
     end
 
-    def color(text, color, bold=false)
-      bold  = bold ? BOLD : ""
-      "#{bold}#{color}#{text}#{CLEAR}"
+    def gem_name_to_module_name
+      name_parts = options[:gem_name].split('-').map { |n| n.split('_') }
+      name_parts.flatten.map(&:capitalize).join
     end
   end
 end
