f09e4a36d3b732e9806579a0b09b0e9ae2ea4892
André Dieb Martins <andre.dieb@gmail.com>
2014-05-27 19:26:49 -0400

    Fix rubocop failures

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -3,12 +3,12 @@ require File.dirname(__FILE__) + '/../lib/gemstrap/cli.rb'
 describe Gemstrap::CLI do
   it 'does raise an error on missing name' do
     expect do
-      Gemstrap::CLI.read([])  
+      Gemstrap::CLI.read([])
     end.to raise_error
   end
 
   context '#read' do
-    let(:args) { {
+    let(:args) do {
       gem_name: 'yourface_jokes',
       description: 'Generates your-face jokes. Guaranteed funny.',
       summary: 'Your-face joke generator',
@@ -17,8 +17,8 @@ describe Gemstrap::CLI do
       authors_emails: ['jd@sacredheart.com'],
       homepage: 'http://yourface.io'
       }
-    }
-    let(:sample_argv) {
+    end
+    let(:sample_argv) do
       ['-n', args[:gem_name],
        '-a', args[:authors].first,
        '-m', args[:authors_emails].first,
@@ -26,7 +26,7 @@ describe Gemstrap::CLI do
        '-s', args[:summary],
        '-g', args[:github_user],
        '-H', args[:homepage]]
-    }
+    end
     let(:options) { Gemstrap::CLI.read(sample_argv) }
 
     it 'does parse input arguments' do
